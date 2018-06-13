106c808ef8c4f7ba5ddb931328df2fcb364001ab
ling.huang <lingling374@qq.com>
2018-06-13 20:48:40 +0800

    format table

--- src/components/Table.vue
+++ src/components/Table.vue
@@ -2,38 +2,92 @@
 * Created by huangling on 11/06/2018.
 */
 <template>
-    <div>
-        <md-table v-model="dataSource" md-card>
+    <div class="container">
+        <md-table v-model="searched" md-card
+                  :md-sort.sync="currentSort" :md-sort-order.sync="currentSortOrder" :md-sort-fn="customSort"
+        >
             <md-table-toolbar>
-                <h1 class="md-title">Users</h1>
+                <h1 class="md-title">Customer</h1>
+                <md-field md-clearable class="md-toolbar-section-end">
+                    <md-input placeholder="请输入" v-model="search" @input="searchOnTable"/>
+                </md-field>
             </md-table-toolbar>
 
-            <md-table-row slot="md-table-row" slot-scope="{ item }">
-                <md-table-cell md-label="ID" md-sort-by="id" md-numeric>{{ item.id }}</md-table-cell>
-                <md-table-cell md-label="Name" md-sort-by="name">{{ item.name }}</md-table-cell>
-                <md-table-cell md-label="Email" md-sort-by="email">{{ item.email }}</md-table-cell>
-                <md-table-cell md-label="Gender" md-sort-by="gender">{{ item.gender }}</md-table-cell>
-                <md-table-cell md-label="Job Title" md-sort-by="title">{{ item.title }}</md-table-cell>
+            <div v-if="loading" class="placeholder">正在查询...</div>
+            <div v-else-if="searched.length === 0" class="placeholder">没有数据</div>
+
+            <md-table-row
+                    slot="md-table-row" slot-scope="{ item }"
+                    md-selectable="multiple" md-auto-select
+            >
+                <md-table-cell md-label="客户名" md-sort-by="name" md-numeric>{{ item.name }}</md-table-cell>
+                <md-table-cell md-label="公司名" md-sort-by="company">{{ item.company }}</md-table-cell>
+                <md-table-cell md-label="地址" md-sort-by="address">{{ item.address }}</md-table-cell>
+                <md-table-cell md-label="联系电话" md-sort-by="mobile">{{ item.mobile }}</md-table-cell>
+                <md-table-cell md-label="关联设备" md-sort-by="machine">{{ item.machine }}</md-table-cell>
             </md-table-row>
         </md-table>
     </div>
 </template>
 
 <script>
+    import Service from '../actions';
+
+    const toLower = (text = '') => {
+        return text.toString().toLowerCase()
+    };
+
     export default {
-        name: 'TableBasic',
-        props: {
-            title: {
-              type: Function
-            },
-            columns: {
-                type: Array,
-                required: true
-            },
-            dataSource: {
-                type: Array,
-                required: true
+        props: ['loading', 'dataSource', 'defaultSort', 'columns'],
+        data: function () {
+            return {
+                currentSort: '',
+                currentSortOrder: 'desc',
+                search: '',
+                searched: [],
             }
         },
+        watch:  {
+            'loading': function () {
+                this.searched = this.dataSource;
+                this.currentSort = this.defaultSort;
+            }
+        },
+        methods: {
+            customSort (value) {
+                return value.sort((a, b) => {
+                    const sortBy = this.currentSort;
+
+                    if (this.currentSortOrder === 'desc') {
+                        return a[sortBy].localeCompare(b[sortBy])
+                    }
+
+                    return b[sortBy].localeCompare(a[sortBy])
+                })
+            },
+            searchOnTable (text) {
+                if (text) {
+                    const vm = this;
+                    this.searched = this.dataSource.filter(row => {
+                        return vm.columns.find(col => {
+                            const value = row[col.dataIndex];
+                            return toLower(value).indexOf(toLower(text)) > -1
+                        });
+                    })
+                } else {
+                    this.searched = this.dataSource;
+                }
+            },
+        }
     }
 </script>
+<style scoped>
+    .container {
+        width: 100%;
+    }
+
+    .placeholder {
+        padding: 20px;
+        text-align: center;
+    }
+</style>
--- src/pages/machine/List.vue
+++ src/pages/machine/List.vue
@@ -2,15 +2,44 @@
 * Created by huangling on 10/06/2018.
 */
 <template>
-    <p>List</p>
+    <div class="container">
+        <p-table :loading="loading" :dataSource="rows"
+                 :columns="columns"
+        >
+
+        </p-table>
+    </div>
 </template>
 
 <script>
-    export default {
+    import PTable from '../../components/Table.vue';
+    import Service from '../../actions/index';
 
+    export default {
+        components: {
+            PTable
+        },
+        data: () => ({
+            rows: [],
+            columns: [
+                {title: '客户名', dataIndex: 'name'},
+                {title: '公司名', dataIndex: 'company'},
+                {title: '地址', dataIndex: 'address'},
+            ],
+            loading: true
+        }),
+        created: function () {
+            const vm = this;
+            Service.getCustomer().then((data) => {
+                vm.rows = data;
+                vm.loading = false;
+            });
+        },
     }
 </script>
 
-<style>
-
+<style scoped="">
+    .container {
+        flex: 1;
+    }
 </style>
